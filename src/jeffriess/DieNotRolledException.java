83039dc95e19f04c00f5a98d34b8c45ba4d19d4c
jeffriess <sjeffries364@gmail.com>
2024-09-12 10:56:01 -0500

    Completed DieNotRolledException

---
+++ src/jeffriess/DieNotRolledException.java
@@ -0,0 +1,17 @@
+/*
+ * Course: CSC1020
+ * Lab 2 - Exceptions
+ * DieNotRolledException class
+ * Name: Stevenson Jeffries
+ * Last Updated: 9.12/2024
+ */
+package jeffriess;
+
+public class DieNotRolledException extends RuntimeException {
+
+
+    @Override
+    public java.lang.String getMessage() {
+        return super.getMessage();
+    }
+}
---
+++ src/jeffriess/Driver.java
@@ -0,0 +1,12 @@
+/*
+ * Course: CSC1020
+ * Lab 2 - Exceptions
+ * DieNotRolledException class
+ * Name: Stevenson Jeffries
+ * Last Updated: 9.12/2024
+ */
+package jeffriess;
+
+public class Driver {
+
+}
